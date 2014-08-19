c9ea13feee6d6767fae9aac2de52ffa55d177cb7
Christian Garbs <mitch@cgarbs.de>
2014-08-20 01:21:23 +0200

    move two-component UI initialization to base class

--- src/de/cgarbs/lib/glue/Binding.java
+++ src/de/cgarbs/lib/glue/Binding.java
@@ -1,5 +1,8 @@
 package de.cgarbs.lib.glue;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 
@@ -10,20 +13,43 @@ import de.cgarbs.lib.exception.GlueException;
 abstract public class Binding
 {
 	protected DataAttribute attribute;
-	protected JLabel jLabel;
+	protected JLabel     jLabel;
+	protected JComponent jData;
 
 	abstract public void syncToView();
 	abstract public void syncToModel() throws DataException;
-	abstract public void addToContainer(JComponent parent, int gridx, int gridy) throws GlueException;  
+
+	public void addToContainer(JComponent parent, int gridx, int gridy) throws GlueException
+	{
+		if (! (parent.getLayout() instanceof GridBagLayout))
+		{
+			throw new GlueException(
+					GlueException.ERROR.LAYOUT_MANAGER_WRONG,
+					parent.getLayout().getClass().getName()
+					);
+		}
+		
+		GridBagConstraints gbc1 = new GridBagConstraints();
+		gbc1.gridx = gridx;
+		gbc1.gridy = gridy;
+		
+		GridBagConstraints gbc2 = new GridBagConstraints();
+		gbc2.gridx = gridx + 1;
+		gbc2.gridy = gridy;
+		
+		parent.add(jLabel, gbc1);
+		parent.add(jData,  gbc2);
+	}
 
 	public Binding(DataAttribute attribute)
 	{
 		this.attribute = attribute;
 		this.jLabel = createJLabel(attribute.getAttributeName());
-		createComponents();
+		this.jData  = createDataEntryComponent();
 	}
 	
-	abstract protected void createComponents();
+	abstract protected JComponent createDataEntryComponent();
+
 	
 	private JLabel createJLabel(String label)
 	{
--- src/de/cgarbs/lib/glue/TextFieldBinding.java
+++ src/de/cgarbs/lib/glue/TextFieldBinding.java
@@ -1,13 +1,9 @@
 package de.cgarbs.lib.glue;
 
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-
 import javax.swing.JComponent;
 import javax.swing.JTextField;
 
 import de.cgarbs.lib.data.DataAttribute;
-import de.cgarbs.lib.exception.GlueException;
 
 public abstract class TextFieldBinding extends Binding
 {
@@ -20,35 +16,9 @@ public abstract class TextFieldBinding extends Binding
 	}
 
 	@Override
-	public void addToContainer(JComponent parent, int gridx, int gridy)
-			throws GlueException
+	protected JComponent createDataEntryComponent()
 	{
-		if (! (parent.getLayout() instanceof GridBagLayout))
-		{
-			throw new GlueException(
-					GlueException.ERROR.BINDING_NOT_IMPLEMENTED,
-					parent.getLayout().getClass().getName()
-					);
-		}
-		
-		GridBagLayout l = (GridBagLayout) parent.getLayout();
-		
-		GridBagConstraints gbc1 = new GridBagConstraints();
-		gbc1.gridx = gridx;
-		gbc1.gridy = gridy;
-		
-		GridBagConstraints gbc2 = new GridBagConstraints();
-		gbc2.gridx = gridx + 1;
-		gbc2.gridy = gridy;
-		
-		l.setConstraints(jLabel, 	 gbc1);
-		l.setConstraints(jTextField, gbc2);
-	}
-
-	@Override
-	protected void createComponents()
-	{
-		jTextField = new JTextField();
+		return jTextField = new JTextField();
 	}
 
 }
