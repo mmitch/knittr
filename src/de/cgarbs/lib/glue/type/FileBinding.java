270e67d92ae3043bd7a95bf8e44f7b8531772b41
Christian Garbs <mitch@cgarbs.de>
2014-08-23 13:24:45 +0200

    add FileChooser button for FileAttributes

--- src/de/cgarbs/lib/glue/type/FileBinding.java
+++ src/de/cgarbs/lib/glue/type/FileBinding.java
@@ -1,8 +1,13 @@
 package de.cgarbs.lib.glue.type;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 
+import javax.swing.JButton;
 import javax.swing.JComponent;
+import javax.swing.JFileChooser;
+import javax.swing.JPanel;
 
 import de.cgarbs.lib.data.DataAttribute;
 import de.cgarbs.lib.exception.DataException;
@@ -10,6 +15,8 @@ import de.cgarbs.lib.glue.TextFieldBinding;
 
 public class FileBinding extends TextFieldBinding
 {
+	protected JButton jButton;
+
 	public FileBinding(DataAttribute attribute)
 	{
 		super(attribute);
@@ -18,7 +25,46 @@ public class FileBinding extends TextFieldBinding
 	@Override
 	public void syncToView()
 	{
-		File value = (File)attribute.getValue();
+		setViewValue((File)attribute.getValue());
+	}
+
+	@Override
+	public void syncToModel() throws DataException
+	{
+		attribute.setValue(getViewValue());
+	}
+
+	@Override
+	protected JComponent createDataEntryComponent()
+	{
+		super.createDataEntryComponent();
+		jTextField.setEditable(false);
+
+		jButton = new JButton("...");
+
+		jButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0)
+			{
+				JFileChooser fc = new JFileChooser();
+				fc.setSelectedFile(getViewValue());
+				if (fc.showDialog(
+						jButton,
+						"choose file") // FIXME i18n
+						== JFileChooser.APPROVE_OPTION)
+				{
+					setViewValue(fc.getSelectedFile());
+				}
+			}
+		});
+
+		JPanel combiPanel = new JPanel();
+		combiPanel.add(jTextField);
+		combiPanel.add(jButton);
+		return combiPanel;
+	}
+
+	private void setViewValue(File value)
+	{
 		if (value == null)
 		{
 			jTextField.setText("");
@@ -29,28 +75,17 @@ public class FileBinding extends TextFieldBinding
 		}
 	}
 
-	@Override
-	public void syncToModel() throws DataException
+	private File getViewValue()
 	{
 		String value = jTextField.getText();
-		if (value == null)
+		if (value == null || value.length() == 0)
 		{
-			attribute.setValue(null);
+			return null;
 		}
 		else
 		{
-			attribute.setValue(new File(value));
+			return new File(value);
 		}
-	}
-
-	@Override
-	protected JComponent createDataEntryComponent()
-	{
-		// FIXME: add FileChooser button
-		// FIXME: add FileChooser handling
-		super.createDataEntryComponent();
-		jTextField.setEditable(false);
-		return jTextField;
 	}
 
 
